e55e59ec187179de625a6b5c2e855db13d1e54b8
PatrykWysocki5140 <estu@onet.pl>
2023-06-07 08:50:38 +0200

    .

---
+++ lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:handyman/providers/themeNotifier.dart';
+import 'package:handyman/screens/splash_screen/splash_screen_page.dart';
+import 'package:handyman/styles/styles.dart';
+import 'package:handyman/widgets/main_bottom_bar/main_bottom_bar_page.dart';
+import 'package:provider/provider.dart';
+import 'package:bloc/bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MultiProvider(
+    providers: [
+      ChangeNotifierProvider<NightMode>.value(value: NightMode()),
+    ],
+    child: MyApp(),
+  ));
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) =>
+
+      /*MultiBlocProvider(
+        providers: [
+          
+          BlocProvider(
+            create: (BuildContext context) => CalendarBloc(),
+          ),
+        ],
+        child: 
+          */
+      Consumer<NightMode>(
+        builder: (context, nightMode, child) => FutureBuilder<ThemeData>(
+            future: nightMode.getTheme(),
+            initialData: Styles.themeData(false),
+            builder:
+                (BuildContext context, AsyncSnapshot<ThemeData> themeData) {
+              return MaterialApp(
+                title: 'Handyman',
+                theme: themeData.data,
+                initialRoute: '/loading',
+                routes: {
+                  '/main': (context) => MainBottomBar(),
+                  '/loading': (context) => const SplashScreenPage("/main"),
+                },
+                debugShowCheckedModeBanner: false,
+              );
+            }),
+      );
+  //);
+}
---
+++ lib/providers/themeNotifier.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../styles/styles.dart';
+
+class NightMode extends ChangeNotifier {
+  final String preferencesKey = "dark_mode_enabled";
+
+  Future<ThemeData> getTheme() => SharedPreferences.getInstance().then(
+      (value) => Styles.themeData(value.getBool(preferencesKey) ?? false));
+
+  Future<bool> get enabled => SharedPreferences.getInstance()
+      .then((value) => value.getBool(preferencesKey) ?? false);
+
+  Future<void> switchTheme() async {
+    await SharedPreferences.getInstance().then((value) => value.setBool(
+        preferencesKey, !(value.getBool(preferencesKey) ?? true)));
+    notifyListeners();
+  }
+}
